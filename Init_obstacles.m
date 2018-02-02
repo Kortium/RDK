373dc1d5652728f5a9362c54fef4cc782a5d97fb
MAIvakin <maksim-ivakin2@mail.ru>
2018-02-02 18:15:41 +0300

    Now displays different types of obstacles

--- Init_obstacles.m
+++ Init_obstacles.m
@@ -2,15 +2,15 @@ function Init_obstacles(obstacles,plot)
     for i = 1:length(obstacles)
         if obstacles(i).Type == 1
             model = Square_graph_model(obstacles(i));
-            squares(i)= line(plot,model(:,1), model(:,2),'color','r','linewidth',2);
+            squares = line(plot,model(:,1), model(:,2),'color','r','linewidth',2);
         end
         if obstacles(i).Type == 2
             model = Triangle_graph_model(obstacles(i));
-            squares(i)= line(plot,model(:,1), model(:,2),'color','r','linewidth',2);
+            squares(i) = line(plot,model(:,1), model(:,2),'color','r','linewidth',2);
         end
         if obstacles(i).Type == 3
             model = Rectangle_graph_model(obstacles(i));
-            squares(i)= line(plot,model(:,1), model(:,2),'color','r','linewidth',2);
+            squares(i) = line(plot,model(:,1), model(:,2),'color','r','linewidth',2);
         end
     end
 end
